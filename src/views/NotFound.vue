b23e458f2fcdf5e7c7f2db4508e77a01f307783f
Jose Burgon <burgonjose@gmail.com>
2020-08-20 12:17:26 -0500

    Adding styles to the NotFound view

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,6 +1,8 @@
 <template>
-  <div class="h-screen w-screen flex justify-center content-center flex-wrap">
-    <h1>404</h1>
+  <div
+    class="h-screen w-screen flex flex-col items-center justify-center content-center flex-wrap"
+  >
+    <h1 class="text-2xl font-bold mb-10">404</h1>
     <p>Página no encontrada</p>
   </div>
 </template>
